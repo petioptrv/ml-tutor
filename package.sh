f366f0ae3cfc3d22901bd7040e52930925073d84
Petio Petrov <petioptrv@icloud.com>
2024-03-21 13:32:29 +0700

    (refactor) Fixes package.sh

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 # Package the ml-tutor directory into a zip file, ignoring all files and directories that start with a dot, as well
 # as __pycache__ directories.
 # Usage: ./package.sh
-cd ml-tutor && zip -r ../ml-tutor.ankiaddon * -x "*/\.*" -x "*/__pycache__/*"
+cd ml-tutor && zip -r ../ml-tutor.ankiaddon * -x "*/\.*" -x "*/__pycache__/*" -x "*/meta.json"
