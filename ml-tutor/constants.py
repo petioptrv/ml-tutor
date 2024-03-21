27598095c3a1998c071a371132a50302ac8a0dfa
Petio Petrov <petioptrv@icloud.com>
2024-03-21 16:44:10 +0700

    (feat) Improved basic note LLM prompt

--- ml-tutor/constants.py
+++ ml-tutor/constants.py
@@ -33,18 +33,34 @@ TUTOR_NAME = "ML-Tutor"
 REPHRASE_CARDS_AHEAD = 3
 NOTE_TEXT_PARSER = "html.parser"
 LLM_NORMAL_NOTE_REPHRASING_FRONT_PROMPT = """
-Given the spaced-repetition note front text: '{note_text}', please attempt to rephrase
-the note front in a way that retains the core information and intent but alters the
-structure and wording. This rephrasing should encourage understanding and recall of the
-concept rather than memorization of the exact structure of the question. If the text is
+Given the spaced-repetition note front text 
+
+'{note_front}'
+
+and back text
+
+'{note_back}'
+
+please attempt to rephrase the note FRONT TEXT only in a way that retains the core
+information and intent but alters the structure and wording.
+This rephrasing should encourage understanding and recall of the concept rather than
+memorization of the exact structure of the question. If the text is
 too ambiguous to rephrase without altering its intended meaning, return an empty string
 without any further explanation why the text is ambiguous.
 """
 LLM_NORMAL_NOTE_REPHRASING_BACK_PROMPT = """
-Given the spaced-repetition note back text: '{note_text}', please attempt to rephrase
-the note back in a way that retains the core information and intent but alters the
-structure and wording. This rephrasing should encourage understanding and recall of the
-concept rather than memorization of the exact structure of the question. If the text is
+Given the spaced-repetition note front text 
+
+'{note_front}'
+
+and back text
+
+'{note_back}'
+
+please attempt to rephrase the note BACK TEXT only in a way that retains the core
+information and intent but alters the structure and wording.
+This rephrasing should encourage understanding and recall of the concept rather than
+memorization of the exact structure of the question. If the text is
 too ambiguous to rephrase without altering its intended meaning, return an empty string
 without any further explanation why the text is ambiguous.
 """
