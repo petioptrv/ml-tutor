e852c8320420372a1616ac9ccba6067361b71568
Petio Petrov <petioptrv@icloud.com>
2024-04-01 13:36:33 +0700

    (refactor) Updates prompts.

--- ml-tutor/constants.py
+++ ml-tutor/constants.py
@@ -33,7 +33,7 @@ TUTOR_NAME = "ML-Tutor"
 REPHRASE_CARDS_AHEAD = 3
 NOTE_TEXT_PARSER = "html.parser"
 LLM_NORMAL_NOTE_REPHRASING_FRONT_PROMPT = """
-Given the spaced-repetition note front text: '{note_text}', please attempt to rephrase
+Given the spaced-repetition note front text: '{note_front}', please attempt to rephrase
 the note front in a way that retains the core information and intent but alters the
 structure and wording. This rephrasing should encourage understanding and recall of the
 concept rather than memorization of the exact structure of the question. If the text is
@@ -41,7 +41,7 @@ too ambiguous to rephrase without altering its intended meaning, return an empty
 without any further explanation why the text is ambiguous.
 """
 LLM_NORMAL_NOTE_REPHRASING_BACK_PROMPT = """
-Given the spaced-repetition note back text: '{note_text}', please attempt to rephrase
+Given the spaced-repetition note back text: '{note_back}', please attempt to rephrase
 the note back in a way that retains the core information and intent but alters the
 structure and wording. This rephrasing should encourage understanding and recall of the
 concept rather than memorization of the exact structure of the question. If the text is
@@ -49,7 +49,7 @@ too ambiguous to rephrase without altering its intended meaning, return an empty
 without any further explanation why the text is ambiguous.
 """
 LLM_CLOZE_NOTE_REPHRASING_PROMPT = """
-Given the spaced-repetition cloze-deletion note '{note_text}', please reword it in a
+Given the spaced-repetition cloze-deletion note '{note_cloze}', please reword it in a
 way that retains the core information and intent but alters the structure and wording.
 The goal is to enhance understanding and recall without relying on the exact structure
 of the question. Keep the same number of fill-in-the-blank spaces. If the text is too
